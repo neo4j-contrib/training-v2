0b6fe3ac8453448dd0008d64957686dd4b9b3853
Ryan Boyd <ryan@ryguy.com>
2018-11-26 11:38:57 -0800

    updated for neo4j.com publishing

--- Courses/IntroToNeo4j/preview.sh
+++ Courses/IntroToNeo4j/preview.sh
@@ -1,11 +1,11 @@
 echo "Rendering Training"
-#asciidoctor -T s adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
-#asciidoctor -T s adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
-#asciidoctor -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
-#asciidoctor -T s adoc/04_IntroductionToCypher.adoc -o html/04_IntroductionToCypher.html
-#asciidoctor -T s adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
-#asciidoctor -T s adoc/06_CreatingNodesAndRelationships.adoc -o html/06_CreatingNodesAndRelationships.html
-#asciidoctor -T s adoc/07_GettingMoreOutOfNeo4j.adoc -o html/07_GettingMoreOutOfNeo4j.html
+asciidoctor -T s adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
+asciidoctor -T s adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
+asciidoctor -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
+asciidoctor -T s adoc/04_IntroductionToCypher.adoc -o html/04_IntroductionToCypher.html
+asciidoctor -T s adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
+asciidoctor -T s adoc/06_CreatingNodesAndRelationships.adoc -o html/06_CreatingNodesAndRelationships.html
+asciidoctor -T s adoc/07_GettingMoreOutOfNeo4j.adoc -o html/07_GettingMoreOutOfNeo4j.html
 
 #asciidoctor-pdf -T s adoc/01_IntroductionToGraphDatabases.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/01_IntroductionToGraphDatabases.pdf
 #sciidoctor-pdf -T s adoc/02_IntroductionToNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/02.yml -o pdf/02_IntroductionToNeo4j.pdf
