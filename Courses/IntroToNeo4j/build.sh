996957e8cc30b7d7fdb22fb455c34195311ccac5
Ryan Boyd <ryan@ryguy.com>
2018-12-01 21:03:12 -0700

    updated UX

--- Courses/IntroToNeo4j/build.sh
+++ Courses/IntroToNeo4j/build.sh
@@ -1,5 +1,7 @@
-echo "Rendering Cypher Training"
-asciidoctor -T _templates cypher/index_60-Minute-Cypher.adoc -o cypher/index_wp.html
-asciidoctor -a image=$IMG -a env-training -T _templates/parts cypher/part*.adoc -D cypher/html/
-echo "Rendering Production Training"
-asciidoctor -T _templates production/index_neo4j_in_production.adoc -o production/index_wp.html
+asciidoctor -T _templates_v2 adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
+asciidoctor -T _templates_v2 adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
+asciidoctor -T _templates_v2 adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
+asciidoctor -T _templates_v2 adoc/04_IntroductionToCypher.adoc -o html/04_IntroductionToCypher.html
+asciidoctor -T _templates_v2 adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
+asciidoctor -T _templates_v2 adoc/06_CreatingNodesAndRelationships.adoc -o html/06_CreatingNodesAndRelationships.html
+asciidoctor -T _templates_v2 adoc/07_GettingMoreOutOfNeo4j.adoc -o html/07_GettingMoreOutOfNeo4j.html
