a71afaa05dd22f0b43977fc33247a55d1694548d
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2018-12-03 10:36:57 -0600

    Modified PDFs per Eve's comment

--- Courses/IntroToNeo4j/preview.sh
+++ Courses/IntroToNeo4j/preview.sh
@@ -1,19 +1,19 @@
 echo "Rendering Training"
 
-asciidoctor -T s adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-asciidoctor -T s adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
-asciidoctor -T s adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
-asciidoctor -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
+#asciidoctor -T s adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
+#asciidoctor -T s adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
+#asciidoctor -T s adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
+#asciidoctor -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
 asciidoctor -T s adoc/04_IntroductionToCypher.adoc -o html/04_IntroductionToCypher.html
-asciidoctor -T s adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
-asciidoctor -T s adoc/06_CreatingNodesAndRelationships.adoc -o html/06_CreatingNodesAndRelationships.html
-asciidoctor -T s adoc/07_GettingMoreOutOfNeo4j.adoc -o html/07_GettingMoreOutOfNeo4j.html
-asciidoctor -T s adoc/08_Summary.adoc -o html/08_Summary.html
+#asciidoctor -T s adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
+#asciidoctor -T s adoc/06_CreatingNodesAndRelationships.adoc -o html/06_CreatingNodesAndRelationships.html
+#asciidoctor -T s adoc/07_GettingMoreOutOfNeo4j.adoc -o html/07_GettingMoreOutOfNeo4j.html
+#asciidoctor -T s adoc/08_Summary.adoc -o html/08_Summary.html
 
 #asciidoctor-pdf -T s adoc/01_IntroductionToGraphDatabases.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/01_IntroductionToGraphDatabases.pdf
 #sciidoctor-pdf -T s adoc/02_IntroductionToNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/02.yml -o pdf/02_IntroductionToNeo4j.pdf
 #asciidoctor-pdf -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/03.yml -o pdf/03_SettingUpYourDevelopmentEnvironment.pdf
-#asciidoctor-pdf -T s adoc/04_IntroductionToCypher.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/04.yml -o pdf/04_IntroductionToCypher.pdf
+asciidoctor-pdf -T s adoc/04_IntroductionToCypher.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/04.yml -o pdf/04_IntroductionToCypher.pdf
 #asciidoctor-pdf -T s adoc/05_GettingMoreOutOfQueries.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/05.yml -o pdf/05_GettingMoreOutOfQueries.pdf
 #asciidoctor-pdf -T s adoc/06_CreatingNodesAndRelationships.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/06.yml -o pdf/06_CreatingNodesAndRelationships.pdf
 #asciidoctor-pdf -T s adoc/07_GettingMoreOutOfNeo4j.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/07.yml   -o pdf/07_GettingMoreOutOfNeo4j.pdf
@@ -21,9 +21,9 @@ asciidoctor -T s adoc/08_Summary.adoc -o html/08_Summary.html
 #open html/01_IntroductionToGraphDatabases.html
 #open html/02_IntroductionToNeo4j.html
 #open html/03_SettingUpYourDevelopmentEnvironment.html
-#open html/04_IntroductionToCypher.html
+open html/04_IntroductionToCypher.html
 #open html/05_GettingMoreOutOfQueries.html
 #open html/06_CreatingNodesAndRelationships.html
 #open html/07_GettingMoreOutOfNeo4j.html
-open html/00_AboutThisCourse.html
-open html/08_Summary.html
+#open html/00_AboutThisCourse.html
+#open html/08_Summary.html
