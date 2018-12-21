9f49536f571e8984d65c2499d5c386a09483a83c
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2018-12-20 18:16:06 -0600

    Changes per Michael's comments and field comments

--- Courses/IntroToNeo4j/preview.sh
+++ Courses/IntroToNeo4j/preview.sh
@@ -10,12 +10,12 @@ echo "Rendering Training"
 #asciidoctor -T _templates_v2 adoc/08_Summary.adoc -o html/08_Summary.html
 
 #asciidoctor-pdf -T s adoc/01_IntroductionToGraphDatabases.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/01_IntroductionToGraphDatabases.pdf
-#sciidoctor-pdf -T s adoc/02_IntroductionToNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/02.yml -o pdf/02_IntroductionToNeo4j.pdf
+#asciidoctor-pdf -T s adoc/02_IntroductionToNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/02.yml -o pdf/02_IntroductionToNeo4j.pdf
 #asciidoctor-pdf -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/03.yml -o pdf/03_SettingUpYourDevelopmentEnvironment.pdf
 #asciidoctor-pdf -T s adoc/04_IntroductionToCypher.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/04.yml -o pdf/04_IntroductionToCypher.pdf
 #asciidoctor-pdf -T s adoc/05_GettingMoreOutOfQueries.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/05.yml -o pdf/05_GettingMoreOutOfQueries.pdf
-#asciidoctor-pdf -T s adoc/06_CreatingNodesAndRelationships.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/06.yml -o pdf/06_CreatingNodesAndRelationships.pdf
-asciidoctor-pdf -T s adoc/07_GettingMoreOutOfNeo4j.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/07.yml   -o pdf/07_GettingMoreOutOfNeo4j.pdf
+asciidoctor-pdf -T s adoc/06_CreatingNodesAndRelationships.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/06.yml -o pdf/06_CreatingNodesAndRelationships.pdf
+#asciidoctor-pdf -T s adoc/07_GettingMoreOutOfNeo4j.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/07.yml   -o pdf/07_GettingMoreOutOfNeo4j.pdf
 
 #open html/01_IntroductionToGraphDatabases.html
 #open html/02_IntroductionToNeo4j.html
