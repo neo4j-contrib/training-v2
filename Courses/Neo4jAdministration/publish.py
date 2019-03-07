1ac901fbb566f6c89d0b71f189acf42560a9d7bc
Ryan Boyd <ryan@ryguy.com>
2019-03-07 12:07:45 -0800

    updated publish script to include landing page

--- Courses/Neo4jAdministration/publish.py
+++ Courses/Neo4jAdministration/publish.py
@@ -3,6 +3,19 @@ import json
 import requests
 import sys
 import os
+import sys, getopt
+from flask import render_template
+
+WP_PAGE_IDS = { "index.part.html": {"dev": 93145, "prod": 92928},
+                "00_AboutThisCourse.html": {"dev": 93147, "prod": 92932},
+                "01_IntroductionToNeo4j.html": {"dev": 93148, "prod": 92935},
+                "02_OverviewOfNeo4jAdministration.html": {"dev": 93149, "prod": 92937},
+                "03_ManagingANeo4jDatabase.html": {"dev": 93150, "prod": 92939},
+                "04_CausalClusteringInNeo4j.html": {"dev": 93151, "prod": 92941},
+                "05_SecurityInNeo4j.html": {"dev": 93152, "prod": 92943},
+                "06_MonitoringNeo4j.html": {"dev": 93153, "prod": 92945},
+                "07_Summary.html": {"dev": 93154, "prod": 92947}
+              }
 
 '''
 Get page content
@@ -28,20 +41,38 @@ def update_wordpress_page(pageId, content):
     # build response for update
     response['content'] = content
     headers['Content-Type'] = 'application/json'
-    print url
+    print "\t%s" % (url)
     pr = requests.post(url, headers=headers, data=json.dumps(response))
      
     return pr.content
 
-if 'PUBLISH_DOCS_USERNAME' in os.environ and 'PUBLISH_DOCS_PASSWORD' in os.environ:
-  pageContent = update_wordpress_page(92932, get_page_content('00_AboutThisCourse.html'))
-  pageContent = update_wordpress_page(92935, get_page_content('01_IntroductionToNeo4j.html'))
-  pageContent = update_wordpress_page(92937, get_page_content('02_OverviewOfNeo4jAdministration.html'))
-  pageContent = update_wordpress_page(92939, get_page_content('03_ManagingANeo4jDatabase.html'))
-  pageContent = update_wordpress_page(92941, get_page_content('04_CausalClusteringInNeo4j.html'))
-  pageContent = update_wordpress_page(92943, get_page_content('05_SecurityInNeo4j.html'))
-  pageContent = update_wordpress_page(92945, get_page_content('06_MonitoringNeo4j.html'))
-  pageContent = update_wordpress_page(92947, get_page_content('07_Summary.html'))
-else:
-  print "Environment varisbles for PUBLISH_DOCS_USERNAME and PUBLISH_DOCS_PASSWORD must be set"
-  sys.exit()
+
+def main(argv):
+  stage = 'dev'
+  try:
+     opts, args = getopt.getopt(argv,"h",['stage='])
+  except getopt.GetoptError:
+     print 'publish.py --stage <stage>'
+     sys.exit(2)
+  for opt, arg in opts:
+     if opt == '-h':
+        print 'publish.py --stage <stage>'
+        sys.exit()
+     elif opt in ("--stage"):
+        stage = arg
+  print 'Stage is "%s"' % (stage)
+
+  if stage <> 'dev' and stage <> 'prod':
+    print "Stages 'prod' + 'dev' are only supported stages currently"
+    sys.exit()
+
+  if 'PUBLISH_DOCS_USERNAME' in os.environ and 'PUBLISH_DOCS_PASSWORD' in os.environ:
+    for key, value in WP_PAGE_IDS.iteritems():
+      print "Publishing %s:" % (key)
+      pageContent = update_wordpress_page(value[stage], get_page_content(key))
+  else:
+    print "Environment varisbles for PUBLISH_DOCS_USERNAME and PUBLISH_DOCS_PASSWORD must be set"
+    sys.exit()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
