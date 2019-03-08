54c4855493ef230cf9d7069a73f235637b89bf8d
Ryan Boyd <ryan@ryguy.com>
2019-03-07 22:54:58 -0800

    changed to publish for specific stage required

--- Courses/Neo4jAdministration/build_publish.sh
+++ Courses/Neo4jAdministration/build_publish.sh
@@ -23,4 +23,4 @@ echo "Publishing---"
 echo "-- copying images"
 aws s3 sync --acl public-read img/ s3://graphacademy.neo4j.com/img/neo4j-administration/
 echo "-- copying wordpress"
-python ./publish.py
+python ./publish.py --stage $STAGE
