c1d359cd51648b0807066f2c258f5d1f41675cd4
Ryan Boyd <ryan@ryguy.com>
2019-03-07 16:40:23 -0800

    update page title and build scripts

--- Courses/Neo4jAdministration/build.sh
+++ Courses/Neo4jAdministration/build.sh
@@ -1,8 +1,8 @@
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/01_IntroductionToNeo4j.adoc -o html/01_IntroductionToNeo4j.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/02_OverviewOfNeo4jAdministration.adoc -o html/02_OverviewOfNeo4jAdministration.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/03_ManagingANeo4jDatabase.adoc -o html/03_ManagingANeo4jDatabase.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/04_CausalClusteringInNeo4j.adoc -o html/04_CausalClusteringInNeo4j.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/05_SecurityInNeo4j.adoc -o html/05_SecurityInNeo4j.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/06_MonitoringNeo4j.adoc -o html/06_MonitoringNeo4j.html
-asciidoctor -a imagedir=$IMG -T _templates_v2 adoc/07_Summary.adoc -o html/07_Summary.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/01_IntroductionToNeo4j.adoc -o html/01_IntroductionToNeo4j.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/02_OverviewOfNeo4jAdministration.adoc -o html/02_OverviewOfNeo4jAdministration.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/03_ManagingANeo4jDatabase.adoc -o html/03_ManagingANeo4jDatabase.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/04_CausalClusteringInNeo4j.adoc -o html/04_CausalClusteringInNeo4j.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/05_SecurityInNeo4j.adoc -o html/05_SecurityInNeo4j.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/06_MonitoringNeo4j.adoc -o html/06_MonitoringNeo4j.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/07_Summary.adoc -o html/07_Summary.html
--- Courses/Neo4jAdministration/publish.py
+++ Courses/Neo4jAdministration/publish.py
@@ -3,7 +3,9 @@ import json
 import requests
 import sys
 import os
+import flask
 import sys, getopt
+import boto3
 from flask import render_template
 
 WP_PAGE_IDS = { "index.part.html": {"dev": 93145, "prod": 92928},
@@ -17,12 +19,32 @@ WP_PAGE_IDS = { "index.part.html": {"dev": 93145, "prod": 92928},
                 "07_Summary.html": {"dev": 93154, "prod": 92947}
               }
 
+fapp = flask.Flask('pub', template_folder='.')
+
 '''
 Get page content
 '''
 def get_page_content(filename):
   file = open('html/%s' % filename)
   return file.read()
+
+'''
+Publish file to S3 and get version
+'''
+def publish_app_js(stage, filename):
+  global API_BASE_URL
+
+  # Create an S3 client
+  s3 = boto3.client('s3')
+
+  with fapp.app_context():
+    tmpl_vars = {'API_BASE_URL': API_BASE_URL[stage], 'STAGE': stage}
+    rendered_content = render_template('js/' + filename, **tmpl_vars)
+
+  f = s3.put_object(Body=bytes(rendered_content), Bucket='cdn.neo4jlabs.com', Key='graphacademy/neo4j-administration/' + stage + '/' + filename, ACL='public-read')
+  print "\t\thttps://cdn.neo4jlabs.com/graphacademy/neo4j-administration/%s/%s?versionId=%s" % (stage, filename, f['VersionId'])
+  return f['VersionId']
+
 
 '''
 Update wordpress page
@@ -67,6 +89,7 @@ def main(argv):
     sys.exit()
 
   if 'PUBLISH_DOCS_USERNAME' in os.environ and 'PUBLISH_DOCS_PASSWORD' in os.environ:
+    # skip publishing JS as we'll publish separately before building web so asciidoc has URLs
     for key, value in WP_PAGE_IDS.iteritems():
       print "Publishing %s:" % (key)
       pageContent = update_wordpress_page(value[stage], get_page_content(key))
