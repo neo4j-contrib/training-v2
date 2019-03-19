c09adf763a8380e178bf14b8547b91b5044ab174
Mark Needham <m.h.needham@gmail.com>
2019-03-19 18:45:47 +0000

    data science training

---
+++ Courses/DataScience/build.sh
@@ -0,0 +1,7 @@
+asciidoctor -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
+asciidoctor -T _templates_v2 adoc/01_IntroductionToDataScienceWithNeo4j.adoc -o html/01_IntroductionToDataScienceWithNeo4j.html
+asciidoctor -T _templates_v2 adoc/02_SettingUpYourDevelopmentEnvironment.adoc -o html/02_SettingUpYourDevelopmentEnvironment.html
+asciidoctor -T _templates_v2 adoc/03_ExploratoryDataAnalysis.adoc -o html/03_ExploratoryDataAnalysis.html
+asciidoctor -T _templates_v2 adoc/04_Predictions.adoc -o html/04_Predictions.html
+asciidoctor -T _templates_v2 adoc/05_Recommendations.adoc -o html/05_Recommendations.html
+asciidoctor -T _templates_v2 adoc/06_Summary.adoc -o html/06_Summary.html
