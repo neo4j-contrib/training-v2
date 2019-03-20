9c71c3b0f0224795f2b07b2fc192bebf03b4e95b
Mark Needham <m.h.needham@gmail.com>
2019-03-20 17:21:01 +0000

    more updates

--- Courses/IntroToNeo4j/preview.sh
+++ Courses/IntroToNeo4j/preview.sh
@@ -1,7 +1,7 @@
 echo "Rendering Training"
 #asciidoctor -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-#asciidoctor -T _templates_v2 adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
-#asciidoctor -T _templates_v2 adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
+asciidoctor -T _templates_v2 adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
+asciidoctor -T _templates_v2 adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
 #asciidoctor -T _templates_v2 adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
 asciidoctor -T _templates_v2 adoc/04_IntroductionToCypher.adoc -o html/04_IntroductionToCypher.html
 #asciidoctor -T _templates_v2 adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
@@ -12,7 +12,7 @@ asciidoctor -T _templates_v2 adoc/04_IntroductionToCypher.adoc -o html/04_Introd
 #asciidoctor-pdf -T s adoc/01_IntroductionToGraphDatabases.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/01_IntroductionToGraphDatabases.pdf
 #asciidoctor-pdf -T s adoc/02_IntroductionToNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/02.yml -o pdf/02_IntroductionToNeo4j.pdf
 #asciidoctor-pdf -T s adoc/03_SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/03.yml -o pdf/03_SettingUpYourDevelopmentEnvironment.pdf
-asciidoctor-pdf -T s adoc/04_IntroductionToCypher.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/04.yml -o pdf/04_IntroductionToCypher.pdf
+#asciidoctor-pdf -T s adoc/04_IntroductionToCypher.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/04.yml -o pdf/04_IntroductionToCypher.pdf
 #asciidoctor-pdf -T s adoc/05_GettingMoreOutOfQueries.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/05.yml -o pdf/05_GettingMoreOutOfQueries.pdf
 #asciidoctor-pdf -T s adoc/06_CreatingNodesAndRelationships.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/06.yml -o pdf/06_CreatingNodesAndRelationships.pdf
 #asciidoctor-pdf -T s adoc/07_GettingMoreOutOfNeo4j.adoc -a allow-uri-read  -a pdf-style=asciidoc-themes/07.yml   -o pdf/07_GettingMoreOutOfNeo4j.pdf
