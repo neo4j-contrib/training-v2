70c886f4541df008ecbfe8af34d6ce0e81b5ddf2
Mark Needham <m.h.needham@gmail.com>
2019-03-20 06:26:51 +0000

    remove unwanted images

--- Courses/DataScience/build.sh
+++ Courses/DataScience/build.sh
@@ -1,7 +1,6 @@
 asciidoctor -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-asciidoctor -T _templates_v2 adoc/01_IntroductionToDataScienceWithNeo4j.adoc -o html/01_IntroductionToDataScienceWithNeo4j.html
-asciidoctor -T _templates_v2 adoc/02_SettingUpYourDevelopmentEnvironment.adoc -o html/02_SettingUpYourDevelopmentEnvironment.html
-asciidoctor -T _templates_v2 adoc/03_ExploratoryDataAnalysis.adoc -o html/03_ExploratoryDataAnalysis.html
+asciidoctor -T _templates_v2 adoc/01_SettingUpYourDevelopmentEnvironment.adoc -o html/01_SettingUpYourDevelopmentEnvironment.html
+asciidoctor -T _templates_v2 adoc/02_ExploratoryDataAnalysis.adoc -o html/02_ExploratoryDataAnalysis.html
+asciidoctor -T _templates_v2 adoc/03_Recommendations.adoc -o html/03_Recommendations.html
 asciidoctor -T _templates_v2 adoc/04_Predictions.adoc -o html/04_Predictions.html
-asciidoctor -T _templates_v2 adoc/05_Recommendations.adoc -o html/05_Recommendations.html
-asciidoctor -T _templates_v2 adoc/06_Summary.adoc -o html/06_Summary.html
+asciidoctor -T _templates_v2 adoc/05_Summary.adoc -o html/05_Summary.html
