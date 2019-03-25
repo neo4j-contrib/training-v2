c400160d773e9d80a117a2f996450c05cef3b5ee
Mark Needham <m.h.needham@gmail.com>
2019-03-25 15:23:55 +0000

    use the newer publishing infrastructure

--- Courses/DataScience/build.sh
+++ Courses/DataScience/build.sh
@@ -1,6 +1,6 @@
-asciidoctor -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-asciidoctor -T _templates_v2 adoc/01_SettingUpYourDevelopmentEnvironment.adoc -o html/01_SettingUpYourDevelopmentEnvironment.html
-asciidoctor -T _templates_v2 adoc/02_ExploratoryDataAnalysis.adoc -o html/02_ExploratoryDataAnalysis.html
-asciidoctor -T _templates_v2 adoc/03_Recommendations.adoc -o html/03_Recommendations.html
-asciidoctor -T _templates_v2 adoc/04_Predictions.adoc -o html/04_Predictions.html
-asciidoctor -T _templates_v2 adoc/05_Summary.adoc -o html/05_Summary.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/01_SettingUpYourDevelopmentEnvironment.adoc -o html/01_SettingUpYourDevelopmentEnvironment.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/02_ExploratoryDataAnalysis.adoc -o html/02_ExploratoryDataAnalysis.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/03_Recommendations.adoc -o html/03_Recommendations.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/04_Predictions.adoc -o html/04_Predictions.html
+asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/05_Summary.adoc -o html/05_Summary.html
--- Courses/DataScience/publish.py
+++ Courses/DataScience/publish.py
@@ -3,6 +3,22 @@ import json
 import requests
 import sys
 import os
+import flask
+import sys, getopt
+import boto3
+from flask import render_template
+
+
+WP_PAGE_IDS = { "index.part.html": {"dev": 94349, "prod": -1},
+                "00_AboutThisCourse.html": {"dev": 94351, "prod": -1},
+                "01_SettingUpYourDevelopmentEnvironment.html": {"dev": 94357, "prod": -1},
+                "02_ExploratoryDataAnalysis.html": {"dev": 94417, "prod": -1},
+                "03_Recommendations.html": {"dev": 94419, "prod": -1},
+                "04_Predictions.html": {"dev": 94421, "prod": -1},
+                "05_Summary.html": {"dev": 94423, "prod": -1}
+              }
+
+fapp = flask.Flask('pub', template_folder='.')
 
 '''
 Get page content
@@ -10,6 +26,24 @@ Get page content
 def get_page_content(filename):
   file = open('html/%s' % filename)
   return file.read()
+
+'''
+Publish file to S3 and get version
+'''
+def publish_app_js(stage, filename):
+  global API_BASE_URL
+
+  # Create an S3 client
+  s3 = boto3.client('s3')
+
+  with fapp.app_context():
+    tmpl_vars = {'API_BASE_URL': API_BASE_URL[stage], 'STAGE': stage}
+    rendered_content = render_template('js/' + filename, **tmpl_vars)
+
+  f = s3.put_object(Body=bytes(rendered_content), Bucket='cdn.neo4jlabs.com', Key='graphacademy/datascience/' + stage + '/' + filename, ACL='public-read')
+  print "\t\thttps://cdn.neo4jlabs.com/graphacademy/datascience/%s/%s?versionId=%s" % (stage, filename, f['VersionId'])
+  return f['VersionId']
+
 
 '''
 Update wordpress page
@@ -28,21 +62,40 @@ def update_wordpress_page(pageId, content):
     # build response for update
     response['content'] = content
     headers['Content-Type'] = 'application/json'
-    print url
+    print "\t%s" % (url)
     pr = requests.post(url, headers=headers, data=json.dumps(response))
-     
+
     return pr.content
 
-if 'PUBLISH_DOCS_USERNAME' in os.environ and 'PUBLISH_DOCS_PASSWORD' in os.environ:
-  pageContent = update_wordpress_page(88656, get_page_content('00_AboutThisCourse.html'))
-  pageContent = update_wordpress_page(88159, get_page_content('01_IntroductionToGraphDatabases.html'))
-  pageContent = update_wordpress_page(88161, get_page_content('02_IntroductionToNeo4j.html'))
-  pageContent = update_wordpress_page(88163, get_page_content('03_SettingUpYourDevelopmentEnvironment.html'))
-  pageContent = update_wordpress_page(88165, get_page_content('04_IntroductionToCypher.html'))
-  pageContent = update_wordpress_page(88167, get_page_content('05_GettingMoreOutOfQueries.html'))
-  pageContent = update_wordpress_page(88169, get_page_content('06_CreatingNodesAndRelationships.html'))
-  pageContent = update_wordpress_page(88171, get_page_content('07_GettingMoreOutOfNeo4j.html'))
-  pageContent = update_wordpress_page(88694, get_page_content('08_Summary.html'))
-else:
-  print "Environment varisbles for PUBLISH_DOCS_USERNAME and PUBLISH_DOCS_PASSWORD must be set"
-  sys.exit()
+
+def main(argv):
+  stage = 'dev'
+  try:
+     opts, args = getopt.getopt(argv,"h",['stage='])
+  except getopt.GetoptError:
+     print 'publish.py --stage <stage>'
+     sys.exit(2)
+  for opt, arg in opts:
+     if opt == '-h':
+        print 'publish.py --stage <stage>'
+        sys.exit()
+     elif opt in ("--stage"):
+        stage = arg
+  print 'Stage is "%s"' % (stage)
+
+  if stage <> 'dev' and stage <> 'prod':
+    print "Stages 'prod' + 'dev' are only supported stages currently"
+    sys.exit()
+
+  if 'PUBLISH_DOCS_USERNAME' in os.environ and 'PUBLISH_DOCS_PASSWORD' in os.environ:
+    # skip publishing JS as we'll publish separately before building web so asciidoc has URLs
+    for key, value in WP_PAGE_IDS.iteritems():
+      print "Publishing %s:" % (key)
+      pageContent = update_wordpress_page(value[stage], get_page_content(key))
+  else:
+    print "Environment varisbles for PUBLISH_DOCS_USERNAME and PUBLISH_DOCS_PASSWORD must be set"
+    sys.exit()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
