a90075c20a62153f382529f8e16d8bc9c47c8377
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2019-05-01 11:18:54 -0500

    Browser Guides for Implementing Modeling part of Modeling course

---
+++ ExerciseGuides/exercise-guides/neo4j-modeling-exercises/render.sh
@@ -0,0 +1,38 @@
+echo "Usage: sh render.sh [publish]"
+GUIDES=neo4j-guides
+
+function render {
+$GUIDES/run.sh Exercises.adoc index.html +1 "$@"
+$GUIDES/run.sh 01.adoc 01.html +1 "$@"
+$GUIDES/run.sh 02.adoc 02.html +1 "$@"
+$GUIDES/run.sh 03.adoc 03.html +1 "$@"
+$GUIDES/run.sh 04.adoc 04.html +1 "$@"
+$GUIDES/run.sh 05.adoc 05.html +1 "$@"
+$GUIDES/run.sh 06.adoc 06.html +1 "$@"
+$GUIDES/run.sh 07.adoc 07.html +1 "$@"
+$GUIDES/run.sh 08.adoc 08.html +1 "$@"
+$GUIDES/run.sh 09.adoc 09.html +1 "$@"
+}
+
+if [ "$1" == "publish" ]; then
+  URL=guides.neo4j.com/neo4j-modeling-exercises
+  render https://$URL -a csv-url=https://guides.neo4j.com/neo4j-modeling-exercises -a env-training
+  if hash aws 2>/dev/null; then
+	  aws s3 cp --acl public-read --recursive --exclude "*" --include "*.html" --include "*.png" --include "*.jpg" --include "*.gif" --include "*.csv" ./ s3://${URL}/
+	  aws s3 cp --acl public-read index.html s3://${URL}
+  else
+   # ${URL}/?cachebust=123456
+  	s3cmd put --recursive -P *.html img s3://${URL}/
+  	s3cmd put -P index.html s3://${URL}
+  fi
+  echo "Publication Done"
+elif [ "$1" == "render-only" ]; then
+  URL=guides.neo4j.com/neo4j-modeling-exercises
+  render http://$URL
+else
+  URL=localhost:8001/neo4j-modeling-exercises
+  render https://$URL -a csv-url=file:/// -a env-training
+  echo "Starting Websever at $URL Ctrl-c to stop"
+  cd ..
+  python ../$GUIDES/http-server.py
+fi
