f45addae71dd502fc2b76f2fc6bf9f9db1ed572c
Ryan Boyd <ryan@ryguy.com>
2019-07-17 16:53:09 -0700

    updated to include google analytics

--- Courses/AppliedGraphAlgorithms/build_publish.sh
+++ Courses/AppliedGraphAlgorithms/build_publish.sh
@@ -12,9 +12,9 @@ while [ "$1" != "" ]; do
 done
 
 echo "Publishing JS---"
-export QUIZES_JS_URL=`python2 publish_js.py --stage $STAGE --file quizes.js`
+export QUIZES_JS_URL=`python publish_js.py --stage $STAGE --file quizes.js`
 echo -e "\t$QUIZES_JS_URL"
-export CLASS_JS_URL=`python2 publish_js.py --stage $STAGE --file class.js`
+export CLASS_JS_URL=`python publish_js.py --stage $STAGE --file class.js`
 echo -e "\t$CLASS_JS_URL"
 
 echo "Building webpages---"
@@ -23,4 +23,4 @@ echo "Publishing---"
 echo "-- copying images"
 aws s3 sync --acl public-read img/ s3://graphacademy.neo4j.com/img/applied-graph-algos/
 echo "-- copying wordpress"
-python2 ./publish.py --stage $STAGE
+python ./publish.py --stage $STAGE
