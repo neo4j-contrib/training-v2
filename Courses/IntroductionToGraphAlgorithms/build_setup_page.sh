2882fbe34c3de5c00af317815bd2d8678a240ba2
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2019-10-11 11:11:14 -0400

    generated setup as PDF for graph algos course

--- Courses/IntroductionToGraphAlgorithms/build_setup_page.sh
+++ Courses/IntroductionToGraphAlgorithms/build_setup_page.sh
@@ -1,5 +1,6 @@
 
 asciidoctor  adoc/SettingUpYourDevelopmentEnvironment.adoc -o html/IntroGraphAlgos_pre-class_Setup.html
 
+asciidoctor-pdf -T s adoc/SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/SettingUpYourDevelopmentEnvironment.pdf
 
 open  html/IntroGraphAlgos_pre-class_Setup.html
