8b9b56de7fa11bf4d4257b965991cd2cd797edc7
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2019-10-21 16:16:11 -0500

    setup pdf for modeling course

--- Courses/Introduction to Graph Data Modeling/build_setup_page.sh
+++ Courses/Introduction to Graph Data Modeling/build_setup_page.sh
@@ -1,5 +1,7 @@
 
 asciidoctor  adoc/SettingUpYourDevelopmentEnvironment.adoc -o html/GraphDataModelingForNeo4j_pre-class_Setup.html
 
+asciidoctor-pdf -T s adoc/SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/SettingUpYourDevelopmentEnvironment_GraphDataModeling.pdf
+
 
 open html/GraphDataModelingForNeo4j_pre-class_Setup.html
