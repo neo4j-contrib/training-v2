e95f0d7c517f1b8b0a5ec33235d1f6178ef35e8c
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2019-11-06 14:18:17 -0600

    Add Neo4j Aura to setup for modeling, advanced cypher

--- Courses/AdvancedCypher/build_setup_page.sh
+++ Courses/AdvancedCypher/build_setup_page.sh
@@ -1,5 +1,8 @@
 
 asciidoctor  adoc/SettingUpYourDevelopmentEnvironment.adoc -o html/AdvancedCypher_pre-class_Setup.html
 
+asciidoctor-pdf -T s adoc/SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/AdvancedCypher_pre-class_Setup.pdf
+
+
 
 open html/AdvancedCypher_pre-class_Setup.html
