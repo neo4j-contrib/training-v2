eccca51cc2d63e6f05d51e88fe130c598aaf21a6
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2019-11-06 12:40:13 -0600

    Use Aura as an option; correction in Intro Browser guide

--- Courses/IntroToNeo4j/build_setup_page.sh
+++ Courses/IntroToNeo4j/build_setup_page.sh
@@ -1,6 +1,8 @@
 
 asciidoctor  adoc/SettingUpYourDevelopmentEnvironment.adoc -o html/IntroNeo4j_pre-class_Setup.html
 
-asciidoctor-pdf -T s adoc/SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/SettingUpYourDevelopmentEnvironment_IntroNeo4j.pdf
+
+
+#asciidoctor-pdf -T s adoc/SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/01.yml -o pdf/SettingUpYourDevelopmentEnvironment_IntroNeo4j.pdf
 
 open html/IntroNeo4j_pre-class_Setup.html
