fcb8d627986dd410246f5ecbabb15de713602a54
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2019-11-13 14:07:54 -0600

    colsolidate building course setup pages

--- Courses/IntroToNeo4j/build_setup_page.sh
+++
@@ -1,8 +0,0 @@
-
-asciidoctor  adoc/SettingUpYourDevelopmentEnvironment.adoc -o html/IntroNeo4j_pre-class_Setup.html
-
-
-
-asciidoctor-pdf -T s adoc/SettingUpYourDevelopmentEnvironment.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/setup.yml -o pdf/SettingUpYourDevelopmentEnvironment_IntroNeo4j.pdf
-
-open html/IntroNeo4j_pre-class_Setup.html
---
+++ Courses/SetUpGuides/build_setup_page.sh
@@ -0,0 +1,15 @@
+asciidoctor  adoc/Setup_IntroNeo4j.adoc -o html/IntroNeo4j_pre-class_Setup.html
+asciidoctor-pdf -T s adoc/Setup_IntroNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/IntroNeo4j.yml -o pdf/SettingUpYourDevelopmentEnvironment_IntroNeo4j.pdf
+open  html/IntroNeo4j_pre-class_Setup.html
+
+asciidoctor  adoc/Setup_GraphDataModeling.adoc -o html/GraphDataModelingForNeo4j_pre-class_Setup.html
+asciidoctor-pdf -T s adoc/Setup_GraphDataModeling.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/GraphDataModeling.yml -o pdf/SettingUpYourDevelopmentEnvironment_GraphDataModeling.pdf
+open  html/GraphDataModelingForNeo4j_pre-class_Setup.html
+
+asciidoctor  adoc/Setup_AdvancedCypher.adoc -o html/AdvancedCypher_pre-class_Setup.html
+asciidoctor-pdf -T s adoc/Setup_AdvancedCypher.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/AdvancedCypher.yml -o pdf/AdvancedCypher_pre-class_Setup.pdf
+open  html/AdvancedCypher_pre-class_Setup.html
+
+asciidoctor  adoc/Setup_IntroGraphAlgos.adoc -o html/IntroGraphAlgos_pre-class_Setup.html
+asciidoctor-pdf -T s adoc/Setup_IntroGraphAlgos.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/IntroGraphAlgos.yml -o pdf/Setup_IntroGraphAlgos.pdf
+open  html/IntroGraphAlgos_pre-class_Setup.html
