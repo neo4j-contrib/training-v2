162014b975f5278c71921ae6c3d5fd1b262da64c
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2020-01-29 11:46:01 -0600

    Intro 4.0 course content

--- Courses/IntroToNeo4j 4.0/build_publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export IMG='https://s3.amazonaws.com/dev.assets.neo4j.com/wp-content/uploads/online/graphdatabases'
-echo "Building---"
-./build.sh
-echo "Publishing---"
-python ./publish.py
