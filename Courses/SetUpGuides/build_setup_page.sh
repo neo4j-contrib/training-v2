ba0ef6d43fc2fd9f6b3eedbe3f66f73a8841cf24
ElaineRosenberg <elaine.rosenberg@neotechnology.com>
2020-04-09 16:46:35 -0500

    correct browser guide

--- Courses/SetUpGuides/build_setup_page.sh
+++ Courses/SetUpGuides/build_setup_page.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 asciidoctor  adoc/Setup_IntroNeo4j.adoc -o html/IntroNeo4j_pre-class_Setup.html
 asciidoctor-pdf -T s adoc/Setup_IntroNeo4j.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/IntroNeo4j.yml -o pdf/SettingUpYourDevelopmentEnvironment_IntroNeo4j.pdf
 open  html/IntroNeo4j_pre-class_Setup.html
@@ -13,3 +14,4 @@ open  html/AdvancedCypher_pre-class_Setup.html
 asciidoctor  adoc/Setup_IntroGraphAlgos.adoc -o html/IntroGraphAlgos_pre-class_Setup.html
 asciidoctor-pdf -T s adoc/Setup_IntroGraphAlgos.adoc -a allow-uri-read -a pdf-style=asciidoc-themes/IntroGraphAlgos.yml -o pdf/Setup_IntroGraphAlgos.pdf
 #open  html/IntroGraphAlgos_pre-class_Setup.html
+
