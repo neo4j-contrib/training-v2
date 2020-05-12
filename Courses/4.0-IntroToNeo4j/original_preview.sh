d81204775a3243688a9cdfc2884a5d696bc8da73
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-12 17:00:30 +0200

    Remove 4.0-IntroToNeo4j (part of training-v3)

--- Courses/4.0-IntroToNeo4j/original_preview.sh
+++
@@ -1,8 +0,0 @@
-echo "Rendering Cypher Training"
-asciidoctor -T s cypher/index_60-Minute-Cypher.adoc -o cypher/index_wp.html
-asciidoctor -T s cypher/part*.adoc -o cypher/
-asciidoctor -a image=$IMG -a env-training -T s/parts cypher/part*.adoc -D cypher/html/
-echo "Rendering Production Training"
-asciidoctor -T s production/index_neo4j_in_production.adoc -o production/index_wp.html
-open cypher/index_wp.html
-echo "open production/index_wp.html"
