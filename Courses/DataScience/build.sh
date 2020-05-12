a5c4a96206586d26e1038133c01317352d4d1c27
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-12 18:34:30 +0200

    Remove templates duplication

--- Courses/DataScience/build.sh
+++ Courses/DataScience/build.sh
@@ -1,6 +1,19 @@
-asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/01_SettingUpYourDevelopmentEnvironment.adoc -o html/01_SettingUpYourDevelopmentEnvironment.html
-asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/02_ExploratoryDataAnalysis.adoc -o html/02_ExploratoryDataAnalysis.html
-asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/03_Recommendations.adoc -o html/03_Recommendations.html
-asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/04_Predictions.adoc -o html/04_Predictions.html
-asciidoctor -a CLASS_JS_URL=$CLASS_JS_URL -a QUIZES_JS_URL=$QUIZES_JS_URL -a imagedir=$IMG -T _templates_v2 adoc/05_Summary.adoc -o html/05_Summary.html
+#!/usr/bin/env bash
+convert () {
+  adoc_file="$1"
+  echo "converting $adoc_file"
+  asciidoctor -a "section-titles=Dev Env,EDA,Recommendations,Predictions,The End" \
+              -a "module-title=Neo4j GraphAcademy: Data Science with Neo4j" \
+              -a "CLASS_JS_URL=${CLASS_JS_URL}" \
+              -a "QUIZES_JS_URL=${QUIZES_JS_URL}" \
+              -a "imagedir=${IMG}" \
+              -T "../_templates_v2" \
+              "adoc/${adoc_file}" -D html
+}
+
+convert "00_AboutThisCourse.adoc"
+convert "01_SettingUpYourDevelopmentEnvironment.adoc"
+convert "02_ExploratoryDataAnalysis.adoc"
+convert "03_Recommendations.adoc"
+convert "04_Predictions.adoc"
+convert "05_Summary.adoc"
