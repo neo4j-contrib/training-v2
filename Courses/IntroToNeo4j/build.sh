1ca34be54a7abaecbba3969e268d0ebd18e3b887
Guillaume Grossetie <ggrossetie@gmail.com>
2020-05-12 19:10:33 +0200

    Use the same templates on the Intro to Neo4j course

--- Courses/IntroToNeo4j/build.sh
+++ Courses/IntroToNeo4j/build.sh
@@ -1,9 +1,19 @@
-asciidoctor -T _templates_v2 adoc/00_AboutThisCourse.adoc -o html/00_AboutThisCourse.html
-asciidoctor -T _templates_v2 adoc/01_IntroductionToGraphDatabases.adoc -o html/01_IntroductionToGraphDatabases.html
-asciidoctor -T _templates_v2 adoc/02_IntroductionToNeo4j.adoc -o html/02_IntroductionToNeo4j.html
-asciidoctor -T _templates_v2 adoc/03_SettingUpYourDevelopmentEnvironment.adoc -o html/03_SettingUpYourDevelopmentEnvironment.html
-asciidoctor -T _templates_v2 adoc/04_IntroductionToCypher.adoc -o html/04_IntroductionToCypher.html
-asciidoctor -T _templates_v2 adoc/05_GettingMoreOutOfQueries.adoc -o html/05_GettingMoreOutOfQueries.html
-asciidoctor -T _templates_v2 adoc/06_CreatingNodesAndRelationships.adoc -o html/06_CreatingNodesAndRelationships.html
-asciidoctor -T _templates_v2 adoc/07_GettingMoreOutOfNeo4j.adoc -o html/07_GettingMoreOutOfNeo4j.html
-asciidoctor -T _templates_v2 adoc/08_Summary.adoc -o html/08_Summary.html
+#!/usr/bin/env bash
+convert () {
+  adoc_file="$1"
+  echo "converting $adoc_file"
+  asciidoctor -a "section-titles=Intro to Graph DBs,Intro to Neo4j,Dev Env,Cypher,Advanced Queries,Creating Data,More,The End" \
+              -a "module-title=Neo4j GraphAcademy: Introduction to Neo4j" \
+              -T "../_templates_v2" \
+              "adoc/${adoc_file}" -D html
+}
+
+convert "00_AboutThisCourse.adoc"
+convert "01_IntroductionToGraphDatabases.adoc"
+convert "02_IntroductionToNeo4j.adoc"
+convert "03_SettingUpYourDevelopmentEnvironment.adoc"
+convert "04_IntroductionToCypher.adoc"
+convert "05_GettingMoreOutOfQueries.adoc"
+convert "06_CreatingNodesAndRelationships.adoc"
+convert "07_GettingMoreOutOfNeo4j.adoc"
+convert "08_Summary.adoc"
